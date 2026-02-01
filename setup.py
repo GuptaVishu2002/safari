d68feaf7bf6c4a9cba69c277edb37c36f15f43b0
Vishu Gupta <vg8892@argo-comp1.princeton.edu>
2026-01-31 21:52:57 -0500

    modify setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="safari",
     version="0.1.0",
     description="Forked research code from HazyResearch/safari",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     include_package_data=True,
 )
