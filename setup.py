dc84decc389b598233520cbd94b59b813944c994
Vishu Gupta <vg8892@argo-comp1.princeton.edu>
2026-01-31 20:16:25 -0500

    add setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="safari",
+    version="0.1.0",
+    description="Forked research code from HazyResearch/safari",
+    packages=find_packages(),
+    include_package_data=True,
+)
